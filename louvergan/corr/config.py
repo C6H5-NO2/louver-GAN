f7abea4bd2437b578dfa68ff3bc3d53fcb499eea
Junbo Xiong <19368807+C6H5-NO2@users.noreply.github.com>
2022-05-09 00:08:32 +0800

    Update config.py

--- louvergan/corr/config.py
+++ louvergan/corr/config.py
@@ -6,7 +6,7 @@ class CorrSolverConfig:
     checkpoint_path: str
     device: str
 
-    n_epoch: int = 350  # 350 is enough for ae
+    n_epoch: int = 800  # 350 is enough for ae; try 800 for cgan
     save_step: int = 100
 
     latent_dim: int = 128
